06fb01e1b128d774c9a7e72afce0d7f2fedfae43
Trina Nixon <TrinaNixon7@gmail.com>
2022-07-20 17:22:08 -0600

    fixed styling and CssFiles title

--- client/src/components/pages/FlashMessage.js
+++ client/src/components/pages/FlashMessage.js
@@ -1,26 +1,18 @@
-import {Snackbar, Button } from "@mui/material";
-import Alert from '@mui/material/Alert'; 
-import React, {useState} from 'react';
-
-
-
+import { Snackbar, Button } from "@mui/material";
+import React, { useState } from "react";
 
 export const MuiSnackbar = () => {
+  const [open, setOpen] = useState(false);
 
-const [open, setOpen] = useState(false)
- 
-
-const handleClose = (event, reason) => {
-  if(reason === 'clickaway'){
-    return
-  }
-  setOpen(false)
-}
-  return(
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+  return (
     <>
-
-      <Button
-        onClick={() => setOpen(true)}>Update Profile</Button>
+      <Button onClick={() => setOpen(true)}>Update Profile</Button>
       <Snackbar
         message="Profile updated successfully"
         autoHideDuration={4000}
@@ -28,5 +20,5 @@ const handleClose = (event, reason) => {
         onClose={handleClose}
       />
     </>
-  )
-}
+  );
+};
